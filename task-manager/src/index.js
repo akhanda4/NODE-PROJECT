17a8b2d54b44bd1052db98300200be824d204c00
akhanda4 <amanhanda420@gmail.com>
2020-01-25 13:15:32 +0530

    uploading files to git

---
+++ task-manager/src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+require("./db/mongoose");
+const multer = require("multer");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 100000
+  },
+  fileFilter(req, file, callback) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return callback(new Error("Please upload a word document"));
+    }
+    callback(undefined, true);
+  }
+});
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send();
+});
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
